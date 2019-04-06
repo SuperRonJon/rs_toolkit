753f86cb2ba910292f590cfa4240cf3771557040
pseute <patrick.seute@gmail.com>
2019-04-06 12:11:12 -0400

    add previous option

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ print("Welcome to RS Toolkit\ntype 'exit' to quit, 'help' for a list of commands
 args = ""
 prev_result = None
 while args != "exit":
+    cont = True
     args = input()
     if args != "exit":
         args = args.split()
         command = args.pop(0)
-        if command == "div":
-            if prev_result is not None:
-                prev_result = commands.div(prev_result, args)
-            else:
-                print("Cannot divide after that command.")
-        else:
-            try:
-                cur_result = getattr(commands, command)(args)
-                if cur_result is not None:
-                    prev_result = cur_result
-            except AttributeError as e:
-                print('No command {} found'.format(command))
+        for i, arg in enumerate(args):
+            if arg == 'prev':
+                if prev_result is not None:
+                    args[i] = prev_result
+                else:
+                    print('No previous result to use')
+                    cont = False
+        try:
+            cur_result = getattr(commands, command)(args)
+            if cur_result is not None:
+                prev_result = cur_result
+        except AttributeError as e:
+            print('No command {} found: {}'.format(command, e))
